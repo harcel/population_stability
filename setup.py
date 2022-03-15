c6f42852bde478d3646e1aaa83965bf88ea5493f
Marcel Haas <m.r.haas@uva.nl>
2022-03-15 15:49:22 +0100

    Minor details updated

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
-# Example!! Still to adapt.
-
 from setuptools import setup
 
 setup(
     name="unstable_populations",
-    version="0.2.0",
+    version="0.2.1",
     author="Marcel Haas, Joris Huese, Lisette Sibbald",
     author_email="datascience@marcelhaas.com",
     packages=["unstable_populations", "unstable_populations.test"],
     url="http://pypi.python.org/pypi/PackageName/",
     license=open("LICENSE").read(),
-    description="An awesome package that does something",
+    description="Package to calculate Unstable Population Indicator and related population stability indices.",
     long_description=open("README.md").read(),
     install_requires=[
         "numpy >= 1.0.0",
